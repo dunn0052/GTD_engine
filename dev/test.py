ef86535e527c8725de68f43fdb6a566f63a48940
Kevin Dunn <dunn0407@umn.edu>
2019-07-09 14:17:50 -0500

    removed reference to local enemy from test

--- dev/test.py
+++ dev/test.py
@@ -16,12 +16,6 @@ s.level.setPC(PC(image = "images//mog.png", x = 10, y =7, spd = 300, frameSpeed 
 watergun = Missile(x = 0, y = 0, image = "images//waterGunOne.png", frames = 1, level = s.level, char = s.level.PC, damage = 1, frameSpeed = 30)
 
 s.level.PC.attatchWeapon(watergun)
-ene = Enemy(x = 0, y = 0, image = "images//csBig.png", frames = 12, cycle = 3, spd = 100, direction = 0, frameSpeed = 10, health = 10, level = s.level)
-ene.move(300,300)
-s.level.enemy_sprites.add(ene)
-s.level.solid_sprites.add(ene)
-s.level.NPC_LAYER.add(ene)
-s.level.all_sprites.add(ene)
-s.level.animated_sprites.add(ene)
+
 
 s.run()
