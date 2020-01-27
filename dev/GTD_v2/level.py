55352351157ed3849c4a2ec6ad522fc8e7c33007
Kevin Dunn <dunn0407@umn.edu>
2020-01-27 07:09:05 -0600

    dev

---
+++ dev/GTD_v2/level.py
@@ -0,0 +1,70 @@
+import pygame as pg
+from textBox import Textbox
+# the level class holds the layers of sprites to draw
+# it also executes commands of the current controller context
+
+
+class Level:
+    def __init__(self, layerNum = 7, name = "NoName"):
+        # controller access
+        self.controllers = []
+        self.tileHeight, self.tileWidth = 0,0
+        # filenames
+        self.name = name
+        # initialize all variables and do all the setup for a new game
+        self.all_sprites = pg.sprite.Group()
+        self.solid_sprites = pg.sprite.Group()
+        self.animated_sprites = pg.sprite.Group()
+        self.static_sprites = pg.sprite.OrderedUpdates() # maybe layeredUpdates?
+        self.text_layer = pg.sprite.GroupSingle()
+        self.npc_sprites = pg.sprite.Group()
+        self.enemy_sprites = pg.sprite.Group()
+        # draw layers
+        self.layers = []
+        for i in range(layerNum):
+            self.layers.append(pg.sprite.LayeredUpdates())
+        try:
+            self.BACKGROUND = self.layers[0] # background image
+            self.WALL_LAYER = self.layers[1] # level walls
+            self.NPC_LAYER = self.layers[2] # draw NPC next
+            self.TRIGGER_LAYER = self.layers[3]
+            self.PC_LAYER = self.layers[4]
+            self.OVER_LAYER = self.layers[5] # things overhead - bridges/roof
+            self.WEATHER_LAYER = self.layers[6] # small alpha effects -- rain, clouds, etc.
+        except:
+            print("Number of layers must be greater than 7")
+
+        self.exit = []
+        self.dt = 0
+
+
+
+    def doCommands(self, context):
+        # get input
+        buttons = self.controllers[0].getInput()
+        for button in buttons:
+            context.doCommand(button)
+
+    def setController(self, controller):
+        self.controllers.append(controller)
+
+    def setControllerContext(self, context):
+        self.context = context
+
+    def setPC(self, PC, x, y):
+        self.PC = PC
+        self.PC.level = self
+        self.PC_LAYER.add(self.PC)
+        self.all_sprites.add(self.PC)
+        self.animated_sprites.add(self.PC)
+        self.PC.moveToTile(x, y)
+        self.setControllerContext(self.PC)
+        if self.PC.weapon:
+            self.PC.weapon.kill()
+            self.PC.weapon.level = self.PC.level
+
+    def displayText(self, text):
+        self.text = Textbox(text = text, backgroundImage = "images//textBackground.png", offset = 65, level = self)
+        self.all_sprites.add(self.text)
+        self.text_layer.add(self.text)
+        self.setControllerContext(self.text)
---
+++ dev/GTD_v2/newSprite.py
@@ -0,0 +1,178 @@
+import pygame, math, sys, os
+vec = pygame.math.Vector2
+
+import keyboardInput as KB
+
+class newSprite(pygame.sprite.Sprite):
+    def __init__(self, image = None, frames=1):
+      pygame.init()
+      pygame.sprite.Sprite.__init__(self)
+      self.images = []
+      if type(image) == str:
+          img = self.loadImage(image)
+      else:
+          img = image.convert_alpha()
+      self.originalWidth = img.get_width() // frames
+      self.originalHeight = img.get_height()
+      frameSurf = pygame.Surface((self.originalWidth, self.originalHeight), pygame.SRCALPHA, 32)
+      x = 0
+      for frameNo in range(frames):
+          frameSurf = pygame.Surface((self.originalWidth, self.originalHeight), pygame.SRCALPHA, 32)
+          frameSurf.blit(img, (x, 0))
+          self.images.append(frameSurf.copy())
+          x -= self.originalWidth
+      self.image = pygame.Surface.copy(self.images[0])
+
+      self.screenRefresh = False
+      self.currentImage = 0
+      self.rect = self.image.get_rect()
+      self.rect.topleft = (0, 0)
+      self.mask = pygame.mask.from_surface(self.image)
+      self.angle = 0
+      self.scale = 1
+      self.subFrame = 0
+      # min ms of a frame
+      self.frameCap = 10
+
+      # commands
+      self.commands = commands = {KB.A:self.doA,KB.B:self.doB,KB.X:self.doX,KB.Y:self.doY,KB.DOWN:self.doDOWN, \
+                                  KB.UP:self.doUP,KB.LEFT:self.doLEFT,KB.RIGHT:self.doRIGHT,KB.L:self.doL, \
+                                  KB.R:self.doR,KB.START:self.doSTART, KB.SELECT:self.doSELECT}
+
+
+
+    def parseColour(self,colour):
+      if type(colour) == str:
+          # check to see if valid colour
+          return pygame.Color(colour)
+      else:
+          colourRGB = pygame.Color("white")
+          colourRGB.r = colour[0]
+          colourRGB.g = colour[1]
+          colourRGB.b = colour[2]
+          return colourRGB
+
+    def loadImage(self,fileName, useColorKey=False):
+      if os.path.isfile(fileName):
+          image = pygame.image.load(fileName)
+          image = image.convert_alpha()
+          # Return the image
+          return image
+      else:
+          raise Exception("Error loading image: " + fileName + " - Check filename and path?")
+
+    def addImage(self, filename):
+      self.images.append(self.loadImage(filename))
+
+    def move(self, xpos, ypos, centre=False):
+      if centre:
+          self.rect.center = [xpos, ypos]
+      else:
+          self.rect.topleft = [xpos, ypos]
+
+    def moveTo(self, x, y):
+        self.x = x
+        self.y = y
+        self.rect.x = self.x
+        self.rect.y = self.y
+
+    def changeImage(self, index):
+      self.currentImage = index
+      if self.angle == 0 and self.scale == 1:
+          self.image = self.images[index]
+      else:
+          self.image = pygame.transform.rotozoom(self.images[self.currentImage], -self.angle, self.scale)
+      oldcenter = self.rect.center
+      self.rect = self.image.get_rect()
+      originalRect = self.images[self.currentImage].get_rect()
+      self.originalWidth = originalRect.width
+      self.originalHeight = originalRect.height
+      self.rect.center = oldcenter
+      self.mask = pygame.mask.from_surface(self.image)
+      if self.screenRefresh:
+          pygame.display.update()
+
+    def touching(self, other):
+        collided = pygame.sprite.collide_mask(self , other)
+        return collided
+
+    def groupTouching(self, group):
+        for sprite in group:
+            if self.touching(sprite):
+                return sprite
+
+    def checkCollision(self, other, center = False):
+        if center:
+            return self.rect.collidepoint(other.rect.center)
+        return self.rect.colliderect(other)
+
+    def nextSpriteImage(self):
+        self.currentImage += 1
+        if self.currentImage > len(self.images) - 1:
+            self.currentImage = 0
+        self.changeImage(self.currentImage)
+
+
+    def prevSpriteImage(self):
+        self.currentImage -= 1
+        if self.currentImage < 0:
+            self.currentImage = len(self.images) - 1
+        self.changeImage(self.currentImage)
+
+    def transformSprite(self, angle, scale, hflip=False, vflip=False):
+        oldmiddle = self.rect.center
+        if hflip or vflip:
+            tempImage = pygame.transform.flip(self.images[self.currentImage], hflip, vflip)
+        else:
+            tempImage = self.images[self.currentImage]
+        if angle != 0 or scale != 1:
+            self.angle = angle
+            self.scale = scale
+            tempImage = pygame.transform.rotozoom(tempImage, -angle, scale)
+        self.image = tempImage
+        self.rect = self.image.get_rect()
+        self.rect.center = oldmiddle
+        self.mask = pygame.mask.from_surface(self.image)
+        if self.screenRefresh:
+            pygame.display.update()
+
+
+
+# virtual functions for button inputs
+    def doCommand(self, button):
+        self.commands[button]()
+
+    def doA(self):
+        pass
+    def doB(self):
+        pass
+    def doX(self):
+        pass
+    def doY(self):
+        pass
+    def doLEFT(self):
+        pass
+    def doRIGHT(self):
+        pass
+    def doUP(self):
+        pass
+    def doDOWN(self):
+        pass
+    def doL(self):
+        pass
+    def doR(self):
+        pass
+    def doSTART(self):
+        pass
+    def doSELECT(self):
+        pass
+
+    def animate(self):
+        pass
+
+# unpack from pkling
+    def unpackSprite(self):
+        pass
+
+    def setLevel(self, level):
+        self.level = level
